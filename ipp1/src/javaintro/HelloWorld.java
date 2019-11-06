016e0a4d2fbaf8dbcb406878d60ab6de17b5ba34
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-11-06 10:35:56 +0900

    じゅぎょうのじゃば

---
+++ ipp1/src/javaintro/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package javaintro;
+
+/**
+ * @author b1018104 村石拓海
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+    }
+}
